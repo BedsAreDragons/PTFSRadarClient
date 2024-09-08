fa7ac735605682de6bf65c2991b103bfdaa7fe0f
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-09-08 12:58:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-require('dotenv').config()
+// netlify/functions/fetch_flight_data.js
+const fetch = require('node-fetch');
 
-const apiread = process.env.READPASS
+exports.handler = async (event) => {
+    const apiread = process.env.READPASS;
 
-const response = await fetch('https://ptfsradar.onrender.com/get_flight_data/', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    // Add other headers if needed
-  },
-  body: JSON.stringify({
-    password: apiread
-  })
-});
+    try {
+        const response = await fetch('https://ptfsradar.onrender.com/get_flight_data/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ password: apiread })
+        });
+
+        const data = await response.json();
+        return {
+            statusCode: response.status,
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
+};
